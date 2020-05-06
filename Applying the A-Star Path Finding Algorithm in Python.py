d945134c13979461f6af0716736ed19ee332bd1c
Royybot <61021381+Royybot@users.noreply.github.com>
2020-05-06 22:23:34 +0530

    Update Applying the A-Star Path Finding Algorithm in Python.py

--- Applying the A-Star Path Finding Algorithm in Python.py
+++ Applying the A-Star Path Finding Algorithm in Python.py
@@ -56,59 +56,33 @@ def heuristic(a, b):
 
 def astar(array, start, goal):
     neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-
     close_set = set()
-
     came_from = {}
-
     gscore = {start: 0}
-
     fscore = {start: heuristic(start, goal)}
-
     oheap = []
-
     heapq.heappush(oheap, (fscore[start], start))
-
     while oheap:
-
         current = heapq.heappop(oheap)[1]
-
         if current == goal:
-
             data = []
-
             while current in came_from:
                 data.append(current)
-
                 current = came_from[current]
-
             return data
 
         close_set.add(current)
 
         for i, j in neighbors:
-
             neighbor = current[0] + i, current[1] + j
-
             tentative_g_score = gscore[current] + heuristic(current, neighbor)
-
             if 0 <= neighbor[0] < array.shape[0]:
-
                 if 0 <= neighbor[1] < array.shape[1]:
-
                     if array[neighbor[0]][neighbor[1]] == 1:
                         continue
-
                 else:
-
-                    # array bound y walls
-
                     continue
-
             else:
-
-                # array bound x walls
-
                 continue
 
             if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
@@ -116,26 +90,18 @@ def astar(array, start, goal):
 
             if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1] for i in oheap]:
                 came_from[neighbor] = current
-
                 gscore[neighbor] = tentative_g_score
-
                 fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
-
                 heapq.heappush(oheap, (fscore[neighbor], neighbor))
-
     return False
 
 
 route = astar(grid, start, goal)
-
 route = route + [start]
-
 route = route[::-1]
-
 print(route)
 
 x_coords = []
-
 y_coords = []
 
 for i in (range(0, len(route))):
@@ -144,23 +110,13 @@ for i in (range(0, len(route))):
     x_coords.append(x)
     y_coords.append(y)
 
-# plotting the ENTIRE map and path
-
 fig, ax = plt.subplots(figsize=(20, 20))
-
 ax.imshow(grid, cmap=plt.cm.Dark2)
-
 ax.scatter(start[1], start[0], marker="x", color="white", s=200)
-
 ax.scatter(goal[1], goal[0], marker="x", color="red", s=200)
-
 ax.plot(y_coords, x_coords, color="black")
-
 plt.xlabel(route)
-
 plt.title("Applying the A* Path Finding Algorithm in Python (2D square grid)")
-
 plt.grid()
-
 plt.show()
 
